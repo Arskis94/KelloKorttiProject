fc2875576e3b9848838f6c7956634c3d825d9470
ThomasHillebrink <thomashillebrink@hotmail.com>
2019-12-17 14:12:37 +0200

    Updated App with basic Components

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import UserDashboard from "./components/UserDashboard.js";
+import LogInScreen from "./components/LogInScreen.js";
 
 const App = () => {
-  return (
-    <div>Hello world</div>
-  )
-}
+  const [activeUserData, setActiveUserData] = useState(undefined); // initialize the page with an empty user state
 
-export default App
+  if (activeUserData) {
+    // if there is an active user, render the user Dashboard
+    return (
+      <div>
+        <UserDashboard activeUserData={activeUserData} />
+      </div>
+    );
+  } else {
+    // if there is no active user, render the LogInScreen instead to set an active user
+    return (
+      <div>
+        <LogInScreen
+          activeUserData={activeUserData}
+          setActiveUserData={setActiveUserData}
+        />
+      </div>
+    );
+  }
+};
+
+export default App;
